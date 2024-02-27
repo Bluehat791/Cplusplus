315ba77c49043276bf260b06bf573957d6ec1a81
Anatoliy Shchepetkov <tolick.shepetkov61@gmail.com>
2024-02-27 21:49:31 +0300

    primitive vector

--- my_vector/my_vector.cpp
+++ my_vector/my_vector.cpp
@@ -1,7 +1,7 @@
-#include <vector>
+#include <memory>
 #include <iostream>
 template <typename T>
-class Vector {
+class My_vector {
 private:
     T* arr;
     size_t sz;
@@ -14,68 +14,109 @@ public:
     {
         return sz;
     }
-    size_t cap()
+
+    size_t capacity()
     {
         return cap;
     }
 
-    void reserve(size_t n) {
-        if(n<=cap) return;
-        newarr = reinterpret_cast<T*>(new int8_t[n*sizeof(T)]) // need some bytes
+    void reserve(size_t n)
+    {
+
+        if (n <= cap) return;
+        T* newarr = reinterpret_cast<T*>(new int8_t[n * sizeof(T)]); // need some bytes
         //T* newarr =  new T[n]; faulty
-        for(size_t i =0;i<sz;++i)
-        {   
+
+        try {
+            std::uninitialized_copy(arr, arr + sz, newarr);
+        }
+        catch (...) {
+            delete[] reinterpret_cast<int8_t*>(newarr); //?
+            throw;
+        }
+        /*size_t i =0;
+        try {
+        for( i =0; i<sz;++i)
+        {
             new(newarr+i) T(arr[i]) //placement new
             newarr[i] == arr[i]
         }
+        } catch(...) {
+            for (size_t j = 0; j < i; ++j)
+            {
+                (newarr+i)->~T();
+            }
+            delete[] reinterpret_cast<int8_t*>(newarr); //?
+            throw;
+        }*/
         for (size_t i = 0; i < sz; ++i)
         {
-            (arr+i)->~T;
+            (arr + i)->~T;
         }
-        
+
         delete[] reinterpret_cast<int8_t*>(arr);
         arr = newarr;
-        cap=n;
+        cap = n;
     }
 
     void resize(size_t n, const T& value = T())
-    {   
-        if (n> cap) reserve(n);
+    {
+        if (n > cap) reserve(n);
         for (size_t i = sz; i < n; ++i)
         {
-            new(arr+i) T(value);
+            new(arr + i) T(value);
 
         }
-        if(n<sz) {
+        if (n < sz) {
             sz = n;
         }
-        
-    }
 
+    }
 
     void push_back(const T& value)
     {
-        if(cap == sz)
+        if (cap == sz)
         {
-            reserve(2*sz);
+            reserve(2 * sz);
         }
-        new(arr+sz) T(value);
+        new(arr + sz) T(value);
         ++sz;
     }
 
-    void pop_back(){
+    void pop_back() {
         --sz;
+        (arr + sz)->~T;
+    }
+
+    T& operator[](size_t i)
+    {
+        return arr[i];
+    }
+
+    const T& operator[](size_t i)
+    {
+        return arr[i];
+    }
+
+    T& at(size_t i)
+    {
+        if (i >= sz)
+            throw std::out_of_range("...");
+        return arr[i];
+    }
+
+    T& front()
+    {
+        return arr[sz];
+    }
+
+    T& back()
+    {
+        return arr[0];
     }
 };
 
 int main()
 {
-    std::vector<int> v;
-    for (int i = 0; i < 10; ++i)
-    {
-        v.push_back(i);
-        std::cout << v.size() << " " << v.capacity() << "\n";
-    }
-    
-        return 0;
+    return 0;
 }
